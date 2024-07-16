793ce15cee36ce792c1ff61239024ffb0d82702b
markovsdima <113052998+markovsdima@users.noreply.github.com>
2024-07-16 18:50:42 +0300

    Initial Commit

---
+++ CircleOfFifths/CircleOfFifthsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleOfFifthsApp.swift
+//  CircleOfFifths
+//
+//  Created by Dmitry Markovskiy on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CircleOfFifthsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleOfFifths/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircleOfFifths
+//
+//  Created by Dmitry Markovskiy on 16.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
